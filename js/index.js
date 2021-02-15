2044de335ad008c2995be15daaac4cf9aaf53f5b
Gloobinours <kusami.louan@gmail.com>
2021-02-15 16:45:35 -0500

    Ajout du papillion, tentative de motionPath

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-(()=>{
+(() => {
 
     const vh = window.innerHeight;
     const vw = window.innerWidth;
@@ -16,22 +16,22 @@
             // toute les pages on une scene de setPin inclu, qui permet de rester devant la page le temps que toute les animations se jouent
             new ScrollMagic.Scene({
                 offset: offset(i),
-                duration: globalPageDuration*vh
+                duration: globalPageDuration * vh
             })
             .setPin(`#page${i}`)
             .addTo(controller);
-    
+
             page.forEach(scnOpt => { // pour chaque option de scene de cette page
                 const scene = new ScrollMagic.Scene({ // on prepare une scene set a l'endroit de la page (qui commence en meme temps que la page s'arrete)
                     offset: offset(i),
-                    duration: globalPageDuration*vh
+                    duration: globalPageDuration * vh
                 });
-    
+
                 Object.keys(scnOpt) // on applique chaque option
-                    .forEach(option=>{
+                    .forEach(option => {
                         scene[option](...scnOpt[option]);
                     });
-    
+
                 scene.addTo(controller);
             });
         });
@@ -63,15 +63,80 @@
             // moulin-1plan (la cycliste) bouge de 15% a droite
             { setTween: ['#moulin-1plan', { x: '15%', ease: Power1.easeInOut }] }
         ],
-        [
-            /// PAGE 3 ///
-            // le village en fond qui se dezoom
-            { setTween: ['#parallax2', { scale: 1.45, ease: Power2.easeInOut }] },
-            // contour qui s'ouvre pour laisser voir le village
-            { setTween: [TweenMax.fromTo('#background2', { scale: 1.05, ease: Power2.easeInOut }, { scale: 1 })] }
-        ],
+        // [
+        //     /// PAGE 3 ///
+        //     // le village en fond qui se dezoom
+        //     { setTween: ['#parallax2', { scale: 1.45, ease: Power2.easeInOut }] },
+        //     // contour qui s'ouvre pour laisser voir le village
+        //     { setTween: [TweenMax.fromTo('#background2', { scale: 1.05, ease: Power2.easeInOut }, { scale: 1 })] }
+        // ],
     ];
 
     apply(pages);
+
+    const papillonFlightPath = {
+        curviness: 1.5,
+        autoRotate: true,
+        values: [
+            {x: 100, y:-20},
+            {x: 300, y: 10},
+            {x: 500, y: 100},
+            {x: 750, y: -100},
+        ]
+    }
+
+    const tween = new TimelineLite();
+
+    
+    tween.add(
+        // TweenMax.to("#test-papillon", 1.2, {
+        //     css: { bezier: papillonFlightPath },
+        //     ease: Power1.easeInOut
+        // })
+        TweenLite.to("#test-papillon", 1.2, {
+            bezier: papillonFlightPath,
+            ease: Power1.easeInOut
+        })
+    );
+
+    // tween.to(".paper-plane", {
+    //     duration: 1,
+    //     ease: "power1.inOut",
+    //     motionPath: {
+    //       path: [{x: 100, y: -20}], // you probably want more points here...or just use an SVG <path>!
+    //       curviness: 2,
+    //       autoRotate: true
+    //     }
+    //   }
+
+    new ScrollMagic.Scene({
+        offset: offset(3),
+        duration: globalPageDuration * vh
+    })
+    .setPin(`#page3`)
+    .addIndicators()
+    .addTo(controller);
+    // setTween: ['#parallax2', { scale: 1.45, ease: Power2.easeInOut }]
+
+    new ScrollMagic.Scene({
+        offset: offset(3),
+        duration: globalPageDuration * vh
+    })
+    .setTween('#parallax2', { scale: 1.45, ease: Power2.easeInOut })
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+        offset: offset(3),
+        duration: globalPageDuration * vh
+    })
+    .setTween(tween)
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+        offset: offset(3),
+        duration: globalPageDuration * vh
+    })
+    .setTween(TweenMax.fromTo('#background2', { scale: 1.05, ease: Power2.easeInOut }, { scale: 1 }))
+    .addTo(controller);
 
 })()
